6905c482ee52caf59b5c38dc4954aa56734209d6
Gautam Kadiresh <gautamkadiresh@gmail.com>
2026-01-11 20:41:51 +0530

    added test for mcp server's list_tool

--- tests/test_task_storage_mcp_server.py
+++ tests/test_task_storage_mcp_server.py
@@ -13,4 +13,11 @@ async def test_add_and_list_tasks(server):
     task_lists = await server.mcp.call_tool("list_tasks", arguments={})
     assert json.loads(task_lists[0].text)["desc"] == "Dummy Task"
     assert json.loads(task_lists[0].text)["id"] == server.id_generator("Dummy Task")
-   
+
+@pytest.mark.asyncio
+async def test_mcp_server_list_tools(server):
+    all_tools = await server.mcp.list_tools()
+    assert len(all_tools) == 2
+    tool_names = [tool.name for tool in all_tools]
+    assert "add_task" in tool_names
+    assert "list_tasks" in tool_names
